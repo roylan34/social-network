dbb863297eab12dcb6a0154ac65d0843ae3a8d1f
Roelan <roelanyburan@gmail.com>
2021-03-18 21:51:02 +0800

    implement encrypting password

--- nodeapi/model/user.js
+++ nodeapi/model/user.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
+var uuid = require("uuid/v4");
+var crypto = require("crypto");
 
 var userSchema = mongoose.Schema({
   name: {
@@ -11,7 +13,7 @@ var userSchema = mongoose.Schema({
     require: true,
     trim: true,
   },
-  password: {
+  hash_password: {
     type: String,
     require: true,
   },
@@ -23,4 +25,39 @@ var userSchema = mongoose.Schema({
   updated_at: Date,
 });
 
+//virtual field
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    //temporary variable _password
+    this._password = password;
+
+    //generate timestamp as secret key or create
+    //ur own salt to easily decrypt for future retrieving plain text password
+    this.salt = uuid();
+
+    //store encrypted password to collection User
+    this.hash_password = this.encrpytPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+//methods
+userSchema.methods = {
+  encrpytPassword: function (password) {
+    if (!password) return "";
+
+    try {
+      const hashed_pass = crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+
+      return hashed_pass;
+    } catch (error) {
+      return "";
+    }
+  },
+};
 module.exports = mongoose.model("user", userSchema);
